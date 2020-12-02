5397489560b396a928060c019accb051d9e9ce6b
Phil McCarthy <philmcc2631@gmail.com>
2020-12-02 14:42:19 -0700

    Add theme_parks show page

--- app/controllers/theme_parks_controller.rb
+++ app/controllers/theme_parks_controller.rb
@@ -2,4 +2,8 @@ class ThemeParksController < ApplicationController
   def index
     @theme_parks = ThemePark.all
   end
+
+  def show
+    @theme_park = ThemePark.find(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   get '/', to: 'welcome#index'
   get '/resorts', to: 'resorts#index'
   get '/themeparks', to: 'theme_parks#index'
+  get '/themeparks/:id', to: 'theme_parks#show'
 end
