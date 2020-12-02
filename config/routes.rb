1997003c48990ef199040fa698cef44118c75907
Phil McCarthy <philmcc2631@gmail.com>
2020-12-02 16:08:36 -0700

    Add feature to edit theme_park records

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,13 @@ Rails.application.routes.draw do
 
   post '/resorts', to: 'resorts#create'
   post '/themeparks', to: 'theme_parks#create'
+  post '/themeparks/:id', to: 'theme_parks#update'
   get '/', to: 'welcome#index'
   get '/resorts', to: 'resorts#index'
   get '/themeparks', to: 'theme_parks#index'
   get '/themeparks/new', to: 'theme_parks#new'
   get '/themeparks/:id', to: 'theme_parks#show'
+  get '/themeparks/:id/edit', to: 'theme_parks#edit'
   get '/resorts/new', to: 'resorts#new'
   get '/resorts/:id', to: 'resorts#show'
 end
