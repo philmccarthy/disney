a3ab0953b706351a54c44a67bb39e5d8881ac864
Phil McCarthy <philmcc2631@gmail.com>
2020-12-04 08:21:39 -0700

    Add test environment gems & setup rspec structure

---
+++ spec/features/theme_parks/show_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+RSpec.describe 'ThemeParks Show Page', type: :feature do
+
+  it 'can see attributes of one theme park' do
+    tp_1 = ThemePark.create(name: 'Magic Kingdom', city: 'Orlando', open: true)
+    tp_2 = ThemePark.create(name: 'Epcot', city: 'Orlando', open: false)
+
+    visit "/themeparks/#{tp_1.id}"
+    
+    expect(page).to have_content(tp_1.name)
+    expect(page).to have_content(tp_1.city)
+    expect(page).to have_content("Open")
+    expect(page).to have_link("Edit", href: "/themeparks/#{tp_1.id}/edit")
+    expect(page).to have_button('Delete Theme Park')
+    
+    visit "/themeparks/#{tp_2.id}"
+    
+    expect(page).to have_content(tp_2.name)
+    expect(page).to have_content(tp_2.city)
+    expect(page).to have_content("Closed")
+    expect(page).to have_link("Edit", href: "/themeparks/#{tp_2.id}/edit")
+    expect(page).to have_button('Delete Theme Park')
+  end
+end
