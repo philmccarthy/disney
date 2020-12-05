a9f9dab46aff4bd3a65d1339b0db79670f5ab8b3
Catherine Dean <catherinemdean15@gmail.com>
2020-12-05 13:47:42 -0700

    update new_spec for resort to interact with form

--- spec/features/resorts/new_spec.rb
+++ spec/features/resorts/new_spec.rb
@@ -9,5 +9,12 @@ RSpec.describe 'Resorts New', type: :feature do
     expect(page).to have_field("Amount of rooms")
     expect(page).to have_field("Vacancy")
     expect(page).to have_button("Create Resort")
+
+    fill_in 'Name', with: 'new_name'
+    fill_in 'Amount of rooms', with: '100'
+    select 'true', from: 'Vacancy'
+    click_button 'Create Resort'
+    save_and_open_page
+    expect(page).to have_content('new_name')
   end
 end
