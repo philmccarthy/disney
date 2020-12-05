5e01a44fe7a28523f1c7ca7f723b8a3c88ccfacc
Catherine Dean <catherinemdean15@gmail.com>
2020-12-05 13:05:34 -0700

    add test delete for vacationers show

--- spec/features/vacationers/show_spec.rb
+++ spec/features/vacationers/show_spec.rb
@@ -41,10 +41,21 @@ RSpec.describe 'Vacationer Show', type: :feature do
     fill_in 'Last name', with: 'last_name'
     select 'true', from: 'Checked in'
     click_button "Edit Vacationer"
-    save_and_open_page
     expect(page).to have_content("first_name")
     expect(page).to have_content("last_name")
     expect(page).to have_content("Checked in: true")
+  end
+
+  it 'deletes vacationers' do
+    resort_1 = Resort.create(name: 'Country Villas', amount_of_rooms: 230, vacancy: true)
+    vacationer_1 = resort_1.vacationers.create!(first_name: "Ruby", last_name: "Dog", checked_in: true)
+
+    visit "/vacationers/#{vacationer_1.id}"
+
+    expect(page).to have_button("Delete")
+    click_link "Delete"
+    save_and_open_page
+    expect(page).to have_no_content("Ruby")
 
   end
 
