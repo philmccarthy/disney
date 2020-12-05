a80e9105c0c09e72040bb6ddc3b631aa7663f8a3
Catherine Dean <catherinemdean15@gmail.com>
2020-12-05 13:42:53 -0700

    update resort edit spec to include interaction with form

--- spec/features/resorts/edit_spec.rb
+++ spec/features/resorts/edit_spec.rb
@@ -13,11 +13,28 @@ RSpec.describe 'Resorts Edit', type: :feature do
     expect(page).to have_field("Vacancy")
     expect(page).to have_button("Edit Resort")
 
+    fill_in 'Name', with: 'updated_name'
+    fill_in 'Amount of rooms', with: '100'
+    select 'true', from: 'Vacancy'
+    click_button 'Edit Resort'
+    expect(page).to have_content('updated_name')
+    expect(page).to have_content('100')
+    expect(page).to have_content('Vacancy')
+
+
     visit "/resorts/#{resort_2.id}/edit"
 
     expect(page).to have_field("Name")
     expect(page).to have_field("Amount of rooms")
     expect(page).to have_field("Vacancy")
     expect(page).to have_button("Edit Resort")
+
+    fill_in 'Name', with: 'updated_name'
+    fill_in 'Amount of rooms', with: '100'
+    select 'true', from: 'Vacancy'
+    click_button 'Edit Resort'
+    expect(page).to have_content('updated_name')
+    expect(page).to have_content('100')
+    expect(page).to have_content('Vacancy')
   end
 end
