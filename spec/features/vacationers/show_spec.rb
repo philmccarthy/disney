4a177426a6ee478ce3f3ca767fca7845f0881464
Catherine Dean <catherinemdean15@gmail.com>
2020-12-05 11:56:09 -0700

    update vacationer show test to include three vacationer pages with attributes

--- spec/features/vacationers/show_spec.rb
+++ spec/features/vacationers/show_spec.rb
@@ -10,20 +10,20 @@ RSpec.describe 'Vacationer Show', type: :feature do
     vacationer_2 = resort_1.vacationers.create!(first_name: "Tina", last_name: "Dog", checked_in: true)
     vacationer_3 = resort_2.vacationers.create!(first_name: "Sam", last_name: "Jones", checked_in: false)
 
-    visit '/vacationers/#{vacationer_1.id}'
+    visit "/vacationers/#{vacationer_1.id}"
 
     expect(page).to have_content(vacationer_1.first_name)
     expect(page).to have_content(vacationer_1.last_name)
     expect(page).to have_content(vacationer_1.checked_in)
 
-    visit '/vacationers/#{vacationer_1.id}'
+    visit "/vacationers/#{vacationer_2.id}"
 
     expect(page).to have_content(vacationer_2.first_name)
     expect(page).to have_content(vacationer_2.last_name)
     expect(page).to have_content(vacationer_2.checked_in)
 
-    visit '/vacationers/#{vacationer_1.id}'
-    
+    visit "/vacationers/#{vacationer_3.id}"
+
     expect(page).to have_content(vacationer_3.first_name)
     expect(page).to have_content(vacationer_3.last_name)
     expect(page).to have_content(vacationer_3.checked_in)
