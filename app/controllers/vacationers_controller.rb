ee61875d6315ec1a2741468aa4bc375a961a85f8
Catherine Dean <catherinemdean15@gmail.com>
2020-12-05 12:58:09 -0700

    create update action for vacationers controller

--- app/controllers/vacationers_controller.rb
+++ app/controllers/vacationers_controller.rb
@@ -10,4 +10,15 @@ class VacationersController < ApplicationController
   def edit
     @vacationer = Vacationer.find(params[:id])
   end
+
+  def update
+    vacationer = Vacationer.find(params[:id])
+    vacationer.update({
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      checked_in: params[:checked_in]
+      })
+      vacationer.save
+      redirect_to "/vacationers/#{vacationer.id}"
+  end
 end
