167e2e2a16978fad56950650078d55868f9400a8
Catherine Dean <catherinemdean15@gmail.com>
2020-12-05 12:43:34 -0700

    create new test for editing vacationers

--- spec/features/vacationers/show_spec.rb
+++ spec/features/vacationers/show_spec.rb
@@ -27,7 +27,25 @@ RSpec.describe 'Vacationer Show', type: :feature do
     expect(page).to have_content(vacationer_3.first_name)
     expect(page).to have_content(vacationer_3.last_name)
     expect(page).to have_content(vacationer_3.checked_in)
+  end
 
+  it 'edits vacationers' do
+    resort_1 = Resort.create(name: 'Country Villas', amount_of_rooms: 230, vacancy: true)
+    vacationer_1 = resort_1.vacationers.create!(first_name: "Ruby", last_name: "Dog", checked_in: true)
+
+    visit "/vacationers/#{vacationer_1.id}"
+
+    expect(page).to have_link("Edit", href: "/vacationers/#{vacationer_1.id}/edit")
+    click_link "Edit"
+    fill_in 'First name', with: 'first_name'
+    fill_in 'Last name', with: 'last_name'
+    select 'true', from: 'Checked in'
+    click_button "Create Vacationer"
+    save_and_open_page
+    expect(page).to have_content("first_name")
+    expect(page).to have_content("last_name")
+    expect(page).to have_content("Checked in: true")
 
   end
+
 end
