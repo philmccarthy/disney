2dbf9fc603d89e692c55cccce0111fb75027c188
Catherine Dean <catherinemdean15@gmail.com>
2020-12-06 15:17:28 -0700

    dry up index action in vacationers_controller

--- app/controllers/vacationers_controller.rb
+++ app/controllers/vacationers_controller.rb
@@ -1,7 +1,6 @@
 class VacationersController < ApplicationController
   def index
-    @vacationers = Vacationer.all
-    @ordered_vacationers = @vacationers.order("created_at").reverse_order
+    @ordered_vacationers = Vacationers.order(created_at: :desc)
   end
 
   def show
