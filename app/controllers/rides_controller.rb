59d1f25016ab72226ee9fd7644ee9c491dcade7d
Phil McCarthy <philmcc2631@gmail.com>
2020-12-06 19:42:46 -0700

    Update rides_controller index to order correctly

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -1,7 +1,9 @@
 class RidesController < ApplicationController
   def index
     if params[:occupants_threshold] && params[:occupants_threshold] != ""
-      @rides = Ride.all.min_occupants(params[:occupants_threshold])
+      @rides = Ride.all
+                   .min_occupants(params[:occupants_threshold])
+                   .order(operational: :desc, created_at: :desc)
     else
       @rides = Ride.order(operational: :desc, created_at: :desc)
     end
