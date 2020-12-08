e7a31398dfffe9cf1ce944ac8c4f6791c9863e1c
Catherine Dean <catherinemdean15@gmail.com>
2020-12-08 15:36:48 -0700

    create new routes for new controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  delete '/resorts/:id', to: 'resorts#destroy'
+  get '/', to: 'welcome#index'
+
   delete '/themeparks/:id', to: 'theme_parks#destroy'
-  delete '/rides/:id', to: 'rides#destroy'
-  post '/resorts', to: 'resorts#create'
   post '/themeparks', to: 'theme_parks#create'
   patch '/themeparks/:id', to: 'theme_parks#update'
-  get '/', to: 'welcome#index'
-  get '/resorts', to: 'resorts#index'
   get '/themeparks', to: 'theme_parks#index'
   get '/themeparks/new', to: 'theme_parks#new'
   get '/themeparks/:id', to: 'theme_parks#show'
@@ -16,20 +13,29 @@ Rails.application.routes.draw do
   get '/themeparks/:id/rides/new', to: 'theme_parks#new_ride'
   post '/themeparks/:id/rides', to: 'theme_parks#create_ride'
   get '/themeparks/:id/edit', to: 'theme_parks#edit'
-  get '/resorts/new', to: 'resorts#new'
-  get '/resorts/:id', to: 'resorts#show'
-  get '/resorts/:id/edit', to: 'resorts#edit'
-  patch '/resorts/:id', to: 'resorts#update'
+
+  delete '/rides/:id', to: 'rides#destroy'
   patch '/rides/:id', to: 'rides#update'
   get '/rides', to: 'rides#index'
   get '/rides/:id', to: 'rides#show'
   get '/rides/:id/edit', to: 'rides#edit'
-  get '/vacationers', to: 'vacationers#index'
+
+  delete '/resorts/:id', to: 'resorts#destroy'
+  post '/resorts', to: 'resorts#create'
+  get '/resorts', to: 'resorts#index'
   get '/resorts/:id/vacationers', to: 'resorts#vacationers'
+  get '/resorts/new', to: 'resorts#new'
+  get '/resorts/:id', to: 'resorts#show'
+  get '/resorts/:id/edit', to: 'resorts#edit'
+  patch '/resorts/:id', to: 'resorts#update'
+
+  get '/vacationers', to: 'vacationers#index'
   get '/vacationers/:id', to: 'vacationers#show'
-  get '/resorts/:id/vacationers/new', to: 'resorts#new_vacationer'
-  post '/resorts/:id/vacationers', to: 'resorts#create_vacationer'
   get '/vacationers/:id/edit', to: 'vacationers#edit'
   patch '/vacationers/:id', to: 'vacationers#update'
   delete '/vacationers/:id', to: 'vacationers#destroy'
+
+  get '/resorts/:id/vacationers/new', to: 'resorts_vacationers#new'
+  post '/resorts/:id/vacationers', to: 'resorts_vacationers#create'
+
 end
