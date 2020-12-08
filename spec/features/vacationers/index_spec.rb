a067de16bee3b3865efe5bb8ae5e6e646cba910d
Catherine Dean <catherinemdean15@gmail.com>
2020-12-07 19:45:54 -0700

    add it has link to vacationer show page

--- spec/features/vacationers/index_spec.rb
+++ spec/features/vacationers/index_spec.rb
@@ -23,12 +23,50 @@ RSpec.describe 'Vacationer Index', type: :feature do
     expect(page).to have_content(vacationer_3.first_name)
     expect(page).to have_content(vacationer_3.last_name)
     expect(page).to have_content(vacationer_3.checked_in)
+  end
+
+  it 'links to edit vacationers' do
+    resort_1 = Resort.create(name: 'Country Villas', amount_of_rooms: 230, vacancy: true)
+    resort_2 = Resort.create(name: 'Seaside Shack', amount_of_rooms: 450, vacancy: false)
+
+    vacationer_1 = resort_1.vacationers.create!(first_name: "Ruby", last_name: "Dog", checked_in: true)
+    vacationer_2 = resort_1.vacationers.create!(first_name: "Tina", last_name: "Dog", checked_in: true)
+    vacationer_3 = resort_2.vacationers.create!(first_name: "Sam", last_name: "Jones", checked_in: false)
+
+    visit '/vacationers'
 
     expect(page).to have_link("Edit", href: "/vacationers/#{vacationer_1.id}/edit")
     expect(page).to have_link("Edit", href: "/vacationers/#{vacationer_2.id}/edit")
     expect(page).to have_link("Edit", href: "/vacationers/#{vacationer_2.id}/edit")
+  end
+
+  it 'can link vacationers' do
+    resort_1 = Resort.create(name: 'Country Villas', amount_of_rooms: 230, vacancy: true)
+    resort_2 = Resort.create(name: 'Seaside Shack', amount_of_rooms: 450, vacancy: false)
+
+    vacationer_1 = resort_1.vacationers.create!(first_name: "Ruby", last_name: "Dog", checked_in: true)
+    vacationer_2 = resort_1.vacationers.create!(first_name: "Tina", last_name: "Dog", checked_in: true)
+    vacationer_3 = resort_2.vacationers.create!(first_name: "Sam", last_name: "Jones", checked_in: false)
+
+    visit '/vacationers'
+
     expect(page).to have_button("Delete Vacationer")
     first(:button, "Delete Vacationer").click
     expect(page).to have_no_content(vacationer_3.first_name)
+  end
+
+  it 'links vacationer name to show' do
+    resort_1 = Resort.create(name: 'Country Villas', amount_of_rooms: 230, vacancy: true)
+    resort_2 = Resort.create(name: 'Seaside Shack', amount_of_rooms: 450, vacancy: false)
+
+    vacationer_1 = resort_1.vacationers.create!(first_name: "Ruby", last_name: "Dog", checked_in: true)
+    vacationer_2 = resort_1.vacationers.create!(first_name: "Tina", last_name: "Dog", checked_in: true)
+    vacationer_3 = resort_2.vacationers.create!(first_name: "Sam", last_name: "Jones", checked_in: false)
+
+    visit '/vacationers'
+
+    expect(page).to have_link("#{vacationer_1.first_name}", href: "/vacationers/#{vacationer_1.id}")
+    expect(page).to have_link("#{vacationer_2.first_name}", href: "/vacationers/#{vacationer_2.id}")
+    expect(page).to have_link("#{vacationer_3.first_name}", href: "/vacationers/#{vacationer_3.id}")
   end
 end
