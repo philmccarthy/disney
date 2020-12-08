1548ee2262e4e5cbb04bf7db30f1592eef3b5fd0
Phil McCarthy <philmcc2631@gmail.com>
2020-12-08 16:15:19 -0700

    Add test for theme_park show links

--- spec/features/theme_parks/index_spec.rb
+++ spec/features/theme_parks/index_spec.rb
@@ -1,15 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'ThemeParks Index Page', type: :feature do
-  it 'can see all theme parks names' do
+  it 'can see all theme parks' do
     tp_1 = create(:theme_park, open: true)
     tp_2 = create(:theme_park, open: false)
 
     visit '/themeparks'
 
     expect(page).to have_content(tp_1.name)
+    expect(page).to have_link('Delete')
     expect(page).to have_content(tp_1.created_at.in_time_zone("America/Denver").strftime("Created on %m/%d/%Y at %I:%M%p %Z"))
-
+    expect(page).to have_link('Edit')
+    expect(page).to have_link('Delete')
     expect(page).to have_content(tp_2.name)
     expect(page).to have_content(tp_2.created_at.in_time_zone("America/Denver").strftime("Created on %m/%d/%Y at %I:%M%p %Z"))
     expect(page).to have_link("New Theme Park", href: '/themeparks/new')
