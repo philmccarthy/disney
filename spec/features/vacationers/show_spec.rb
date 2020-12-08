859e9f7be5dd5d37c26d7824910f68d0f0a5e59f
Catherine Dean <catherinemdean15@gmail.com>
2020-12-07 20:32:57 -0700

    update spec/features/vacataioners files to use factorybot

--- spec/features/vacationers/show_spec.rb
+++ spec/features/vacationers/show_spec.rb
@@ -3,13 +3,10 @@ require 'rails_helper'
 RSpec.describe 'Vacationer Show', type: :feature do
 
   it 'shows vacationer with attributes' do
-    resort_1 = Resort.create(name: 'Country Villas', amount_of_rooms: 230, vacancy: true)
-    resort_2 = Resort.create(name: 'Seaside Shack', amount_of_rooms: 450, vacancy: false)
-
-    vacationer_1 = resort_1.vacationers.create!(first_name: "Ruby", last_name: "Dog", checked_in: true)
-    vacationer_2 = resort_1.vacationers.create!(first_name: "Tina", last_name: "Dog", checked_in: true)
-    vacationer_3 = resort_2.vacationers.create!(first_name: "Sam", last_name: "Jones", checked_in: false)
-
+    vacationer_1 = create(:vacationer)
+    vacationer_2 = create(:vacationer)
+    vacationer_3 = create(:vacationer)
+    
     visit "/vacationers/#{vacationer_1.id}"
 
     expect(page).to have_content(vacationer_1.first_name)
