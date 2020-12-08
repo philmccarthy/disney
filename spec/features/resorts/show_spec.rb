09959e7b173e9e53dcd7d790209d35a3ea2466e0
Catherine Dean <catherinemdean15@gmail.com>
2020-12-07 19:56:51 -0700

    create test for link to resorts vacationers from resorts show

--- spec/features/resorts/show_spec.rb
+++ spec/features/resorts/show_spec.rb
@@ -22,4 +22,14 @@ RSpec.describe 'Resorts Show Page', type: :feature do
     expect(page).to have_link("Edit", href: "/resorts/#{resort_2.id}/edit")
     expect(page).to have_button("Delete Resort")
   end
+
+  it 'has link to resorts vacationers page' do
+    resort_1 = Resort.create(name: 'Country Villas', amount_of_rooms: 230, vacancy: true)
+
+    vacationer_1 = resort_1.vacationers.create!(first_name: "Ruby", last_name: "Dog", checked_in: true)
+    vacationer_2 = resort_1.vacationers.create!(first_name: "Tina", last_name: "Dog", checked_in: true)
+
+    visit "/resorts/#{resort_1.id}"
+    expect(page).to have_link("Vacationers at #{resort_1.name}", href: "/resorts/#{resort_1.id}/vacationers")
+  end
 end
