ec0ea2bb87618b22e9eb52fb158dde2b5e342dde
Catherine Dean <catherinemdean15@gmail.com>
2020-12-09 15:04:51 -0700

    create elsif in resorts_controller

--- app/controllers/resorts_controller.rb
+++ app/controllers/resorts_controller.rb
@@ -1,13 +1,16 @@
 class ResortsController < ApplicationController
 
   def index
+    # require "pry"; binding.pry
       if params[:number_of_rooms]
         @ordered_resorts = Resort.where("amount_of_rooms > ?", params[:number_of_rooms])
-      elsif params[:commit]
+      elsif params[:commit] == "Sort by number of vacationers"
         @ordered_resorts = Resort.select("resorts.*, count(vacationers) as vacationers_count")
                                 .joins(:vacationers)
                                 .group(:id)
                                 .order("vacationers_count DESC, vacancy DESC, created_at DESC")
+      elsif params[:exact_match]
+        @ordered_resorts = Resort.where("name like ?", params[:exact_match])
       else
         @ordered_resorts = Resort.order(vacancy: :desc, created_at: :desc)
       end
