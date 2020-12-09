97a7d253046778bfb7c1c25f2152bd90bb8ba322
Catherine Dean <catherinemdean15@gmail.com>
2020-12-09 15:55:01 -0700

    create elsif statment in index for parital and exact keywords for rides index

--- app/controllers/rides_controller.rb
+++ app/controllers/rides_controller.rb
@@ -4,6 +4,10 @@ class RidesController < ApplicationController
       @rides = Ride.all
                    .min_occupants(params[:occupants_threshold])
                    .order(operational: :desc, created_at: :desc)
+    elsif params[:exact_match]
+      @rides = Ride.where("name like ?", params[:exact_match])
+    elsif params[:partial_match]
+      @rides = Ride.where("name like ?", "%#{params[:partial_match]}%")
     else
       @rides = Ride.order(operational: :desc, created_at: :desc)
     end
@@ -12,7 +16,7 @@ class RidesController < ApplicationController
   def show
     @ride = Ride.find(params[:id])
   end
-  
+
   def edit
     @ride = Ride.find(params[:id])
   end
