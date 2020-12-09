4d1650ea579f2592ee6fc2aa8c6ee367ea6fb47b
Phil McCarthy <philmcc2631@gmail.com>
2020-12-08 17:50:14 -0700

    Update feature tests to interact with sort & alphabetize

--- spec/features/theme_parks/index_spec.rb
+++ spec/features/theme_parks/index_spec.rb
@@ -1,19 +1,49 @@
 require 'rails_helper'
 
+require 'rails_helper'
+
+RSpec.describe '', type: :feature do
+  describe '' do
+    before(:each) do
+
+    end
+
+    it '' do
+    end
+  end
+end
+
 RSpec.describe 'ThemeParks Index Page', type: :feature do
-  it 'can see all theme parks' do
-    tp_1 = create(:theme_park, open: true)
-    tp_2 = create(:theme_park, open: false)
+  describe 'themeparks index' do
+    before(:each) do
+    @tp_1 = create(:theme_park, open: true)
+    @tp_2 = create(:theme_park, open: false)
+    create_list(:ride, 5, theme_park: @tp_1)
+    create_list(:ride, 10, theme_park: @tp_2)
+    end
+  
+    it 'can see all theme parks' do
+      visit '/themeparks'
 
-    visit '/themeparks'
+      expect(page).to have_content(@tp_1.name)
+      expect(page).to have_link('Delete')
+      expect(page).to have_content(@tp_1.created_at.in_time_zone("America/Denver").strftime("Created on %m/%d/%Y at %I:%M%p %Z"))
+      expect(page).to have_link('Edit')
+      expect(page).to have_link('Delete')
+      expect(page).to have_content(@tp_2.name)
+      expect(page).to have_content(@tp_2.created_at.in_time_zone("America/Denver").strftime("Created on %m/%d/%Y at %I:%M%p %Z"))
+      expect(page).to have_link("New Theme Park", href: '/themeparks/new')
+    end
 
-    expect(page).to have_content(tp_1.name)
-    expect(page).to have_link('Delete')
-    expect(page).to have_content(tp_1.created_at.in_time_zone("America/Denver").strftime("Created on %m/%d/%Y at %I:%M%p %Z"))
-    expect(page).to have_link('Edit')
-    expect(page).to have_link('Delete')
-    expect(page).to have_content(tp_2.name)
-    expect(page).to have_content(tp_2.created_at.in_time_zone("America/Denver").strftime("Created on %m/%d/%Y at %I:%M%p %Z"))
-    expect(page).to have_link("New Theme Park", href: '/themeparks/new')
+    it 'changes view when sort by number of rides is clicks' do
+      visit '/themeparks'
+
+      expect(@tp_1.name).to appear_before(@tp_2.name)
+
+      click_on "Sort by Number of Rides"
+      
+      expect(page).to have_content("Number of Rides")
+      expect(@tp_2.name).to appear_before(@tp_1.name)
+    end
   end
 end
