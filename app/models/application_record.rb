9399fcc80c48041bf3cdd4039da1ab50037e127a
Catherine Dean <catherinemdean15@gmail.com>
2020-12-09 16:07:05 -0700

    create exact_name_match and partial_name_match methods to be inherited

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,11 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+
+  def self.exact_name_match(keyword)
+    where("name like ?", keyword)
+  end
+
+  def self.partial_name_match(keyword)
+    where("name like ?", "%#{keyword}%")
+  end
 end
